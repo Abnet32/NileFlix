1f5919abb68a60ff86aa94f945929d093ce9660f
Abnet Mekonen <abnetmekonen32@gmail.com>
2026-02-05 05:43:08 -0500

    fear next.js declaration>
    git commit -m:feat next.js start

---
+++ OneDrive/Desktop/General/my code project/abe/app/Login/page.tsx
@@ -0,0 +1,9 @@
+import { LoginForm } from "@/components/login-form";
+
+export default function Home() {
+  return (
+    <div className="container mx-auto flex flex-col items-center justify-center ">
+      <LoginForm />
+    </div>
+  );
+}
