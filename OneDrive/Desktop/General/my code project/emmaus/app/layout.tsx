e483c931aa50ae2a7bee203d1312e3880ec68740
Abnet Mekonen <abnetmekonen32@gmail.com>
2026-02-10 14:08:17 -0500

    feat:shaden ui

--- OneDrive/Desktop/General/my code project/emmaus/app/layout.tsx
+++ OneDrive/Desktop/General/my code project/emmaus/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Outfit } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "@/components/theme-provider";
 
 const outfit = Outfit({
   weight: "400",
@@ -19,8 +20,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${outfit.className}`}>{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <body className={`${outfit.className}`}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
