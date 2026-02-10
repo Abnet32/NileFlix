6010925283969b9f6a3e0af7d64a6df18ba8eb61
Abnet Mekonen <abnetmekonen32@gmail.com>
2026-02-10 14:08:17 -0500

    feat:shaden ui

--- OneDrive/Desktop/General/my code project/emmaus/app/page.tsx
+++ OneDrive/Desktop/General/my code project/emmaus/app/page.tsx
@@ -1,8 +1,11 @@
-
+import { Button } from "@/components/ui/button";
+import { ModeToggle } from "@/components/toggle-theme";
 export default function Home() {
   return (
-    <div>
+    <div className="gap-4 flex flex-col items-center justify-center">
       <h1 className="text-3xl p-10">Emmaus Diary</h1>
+      <Button>Emmaus</Button>
+      <ModeToggle/>
     </div>
   );
 }
