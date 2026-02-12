e566d9b39759d34c6e97ac34af92e0cb416b2b46
Abnet Mekonen <abnetmekonen32@gmail.com>
2026-02-12 04:09:09 -0500

    feat: sign-in form and autntication

--- OneDrive/Desktop/General/my code project/emmaus/app/page.tsx
+++ OneDrive/Desktop/General/my code project/emmaus/app/page.tsx
@@ -1,11 +1,35 @@
+'use client';
 import { Button } from "@/components/ui/button";
 import { ModeToggle } from "@/components/toggle-theme";
+import { useRouter } from "next/navigation";
+import { authClient } from "@/lib/auth-client";
+import { toast } from "sonner";
 export default function Home() {
+  const router = useRouter();
+  const { data: session } = authClient.useSession();
+  async function signOut() {
+    await authClient.signOut({
+      fetchOptions: {
+        onSuccess: () => {
+          router.push("/"); // redirect to login page
+          toast.success("Successfully signed out!");
+        },
+      },
+    });
+  }
   return (
     <div className="gap-4 flex flex-col items-center justify-center">
       <h1 className="text-3xl p-10">Emmaus Diary</h1>
       <Button>Emmaus</Button>
-      <ModeToggle/>
+      <ModeToggle />
+      {session ? (
+        <div>
+          <p>Hi, {session.user.name}!</p>
+          <Button onClick={signOut}>Logout</Button>
+        </div>
+      ) : (
+        <Button>Login</Button>
+      )}
     </div>
   );
 }
