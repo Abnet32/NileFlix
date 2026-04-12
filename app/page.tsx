c8723ff122274992d2589fb2bb080ccb08e8c88a
Abnet Mekonen Gosaye <abnetmekonen32@gmail.com>
2026-04-12 16:34:14 +0300

    refactore

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,9 @@ export default function Home() {
       <p className="text-center mt-4 text-lg">
         Your one-stop destination for streaming movies and TV shows.
       </p>
-      
+      <p>
+        Explore our vast library of content, from the latest releases to timeless classics. Sign up now to start your free trial and enjoy unlimited entertainment on NileFlix! 
+      </p>
     </div>
   );
 }
