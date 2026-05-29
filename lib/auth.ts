6adfb1276b1b5ff0b2dd702972c7fffe52585597
Abnet Mekonen <abnetmekonen32@gmail.com>
2026-05-29 19:03:33 +0300

    feat: implement authentication module with betterAuth and MongoDB adapter

---
+++ lib/auth.ts
@@ -0,0 +1,20 @@
+import { betterAuth } from "better-auth";
+import { mongodbAdapter } from "better-auth/adapters/mongodb";
+import { client, db } from "@/lib/db";
+
+export const auth = betterAuth({
+  database: mongodbAdapter(db, { client }),
+  emailAndPassword: {
+    enabled: true,
+  },
+  socialProviders: {
+    github: {
+      clientId: process.env.GITHUB_CLIENT_ID as string,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+    },
+     google: {
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    },
+  },
+});
