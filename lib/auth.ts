75a27239a038ad9b3a2e24e107c69712cd0fb8d3
Abnet Mekonen <abnetmekonen32@gmail.com>
2026-06-02 17:10:44 +0300

    feat: implement authentication setup using better-auth with MongoDB adapter

---
+++ lib/auth.ts
@@ -0,0 +1,12 @@
+// lib/auth.ts
+import "server-only";
+import { betterAuth } from "better-auth";
+import { mongodbAdapter } from "better-auth/adapters/mongodb";
+import client from "./db";
+
+export const auth = betterAuth({
+  database: mongodbAdapter(client.db()),
+  emailAndPassword: {
+    enabled: true,
+  },
+});
