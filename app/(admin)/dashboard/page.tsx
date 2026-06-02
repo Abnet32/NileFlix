a7205d5ccef3784fcf4097e5b073ec71bb7b21dc
Abnet Mekonen <abnetmekonen32@gmail.com>
2026-06-02 18:03:36 +0300

    feat: implement sign-out functionality in Dashboard component

--- app/(admin)/dashboard/page.tsx
+++ app/(admin)/dashboard/page.tsx
@@ -1,6 +1,21 @@
 "use client";
 
+import { authClient } from "@/lib/auth-client";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+
 const Dashboard = () => {
+  const router = useRouter();
+  useEffect(() => {
+    authClient.signOut({
+      fetchOptions: {
+        onSuccess: () => {
+          router.push("/sign-in");
+        },
+      },
+    });
+  }, [router]);
+
   return <div>Admin Dashboard</div>;
 };
 
