3e2f4b9d23515d4b19e029b97d48fbbc798f2e93
Abnet Mekonen <abnetmekonen32@gmail.com>
2026-06-02 17:46:06 +0300

    refactor: rename dashboard component and improve structure with client-side rendering

--- app/(admin)/dashboard/page.tsx
+++ app/(admin)/dashboard/page.tsx
@@ -1,5 +1,22 @@
-const dashboard = () => {
+"use client";
+
+import { authClient } from "@/lib/auth-client";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+
+const Dashboard = () => {
+  const router = useRouter();
+  useEffect(() => {
+    authClient.signOut({
+      fetchOptions: {
+        onSuccess: () => {
+          router.push("/sign-in");
+        },
+      },
+    });
+  }, [router]);
+
   return <div>Admin Dashboard</div>;
 };
 
-export default dashboard;
+export default Dashboard;
