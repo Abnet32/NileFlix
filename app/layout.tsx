3855108e2387fcd863af057597ac6b171582cd80
Abnet Mekonen <abnetmekonen32@gmail.com>
2026-06-03 22:00:12 +0300

    feat: integrate SidebarProvider and AppSidebar into RootLayout for improved navigation

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import { Outfit } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 import { ThemeProvider } from "@/components/theme-provider";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { AppSidebar } from "@/components/app-sidebar";
 
 const outfit = Outfit({ subsets: ["latin"], variable: "--font-sans" });
 
@@ -34,7 +36,13 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          {children}
+          <SidebarProvider>
+            <AppSidebar />
+            <main>
+              <SidebarTrigger />
+              {children}
+            </main>
+          </SidebarProvider>{" "}
         </ThemeProvider>
       </body>
     </html>
