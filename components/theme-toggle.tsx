4ebcbfee9898cf353f130c569bc25f317f506b76
Abnet Mekonen <abnetmekonen32@gmail.com>
2026-06-08 18:41:25 +0300

    fix:functon error

--- components/theme-toggle.tsx
+++ components/theme-toggle.tsx
@@ -4,8 +4,7 @@ import * as React from "react";
 import { Moon, Sun } from "lucide-react";
 import { useTheme } from "./theme-provider";
 
-export function 
-() {
+export function ModeToggle() {
   const { theme, setTheme } = useTheme();
 
   const toggleTheme = () => {
