855f3ca5e88d2a65126f2dac4991f2a9ec504264
Abnet Mekonen <abnetmekonen32@gmail.com>
2026-06-11 18:14:41 +0300

    chore:add feature filter

--- components/dashboard-search.tsx
+++ components/dashboard-search.tsx
@@ -1,10 +1,20 @@
 import MovieSearch from "@/components/movie-search";
 
-export default function DashboardSearch() {
+type DashboardSearchProps = {
+  scope?: "all" | "movie" | "tv" | "anime";
+};
+
+export default function DashboardSearch({
+  scope = "all",
+}: DashboardSearchProps) {
   return (
     <div className="px-3 pt-4 sm:px-5">
       <div className="max-w-xl">
-        <MovieSearch compact hrefPrefix="/dashboard" />
+        <MovieSearch
+          compact
+          hrefPrefix="/dashboard"
+          defaultScope={scope}
+        />
       </div>
     </div>
   );
