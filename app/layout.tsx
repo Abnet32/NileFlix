d36401e865493a4b3daae22a7007ecbcb3e0d406
Abnet Mekonen <abnetmekonen32@gmail.com>
2026-06-22 09:38:46 +0300

    feat:add metadata graph

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,12 +11,28 @@ const lexend = Lexend_Deca({
 });
 
 export const metadata: Metadata = {
+  metadataBase: new URL("https://nile-flix.vercel.app"),
   title: "NileFlix",
   description:
-    "A free movie discovery app built with Next.js, Tailwind CSS, and TypeScript.",
+    "A free movie discovery website built with Next.js, Tailwind CSS, and TypeScript.",
   icons: {
     icon: "/icon.svg",
     apple: "/icon.svg",
+  },
+  robots: { index: true, follow: true },
+  openGraph: {
+    title: "nileflix",
+    description: "free movie discovery website.",
+    type: "website",
+    images: {
+      url: "/opengraph.png",
+      width: 1200,
+      height: 630,
+      alt: "nileflix",
+    },
+  },
+  twitter: {
+    card: "summary_large_image",
   },
 };
 
